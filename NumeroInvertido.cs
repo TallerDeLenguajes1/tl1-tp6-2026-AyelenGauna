bdd9acf9e082b847f9af32c65cfe1d3f464cac27
Dana Ayelen Gauna <ayelengauna564@gmail.com>
2026-05-19 15:54:05 -0300

    Ejercicio 1

---
+++ NumeroInvertido.cs
@@ -0,0 +1,31 @@
+int numero = 0, longitudNumero;
+string textoNumero;
+bool resultado;
+
+do
+{
+    Console.WriteLine("Ingrese un número mayor a 0");
+    textoNumero = Console.ReadLine();
+
+    resultado = int.TryParse(textoNumero, out numero); //se guarda en valor numérico en la variable "numero"
+
+    if (resultado && numero > 0) 
+    {
+        //Obtengo la longitud del número ingresado
+        longitudNumero = textoNumero.Length;
+        string numeroInvertido = "";
+
+        //Invierto el número ingresado
+        for (int i = 0; i < longitudNumero; i++)
+        {
+            numeroInvertido = numeroInvertido + textoNumero[longitudNumero-1-i];
+        }
+
+        Console.WriteLine($"\nEl número invertido es: {numeroInvertido}");
+
+    }else
+    {
+        Console.WriteLine("\nEl texto ingresado NO es un numero o es menor o igual 0\n");
+    }
+
+} while (!resultado || numero <= 0);
