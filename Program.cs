3c47c9d58e62fed7984fe0d16e27c9eb88f9e95b
Dana Ayelen Gauna <ayelengauna564@gmail.com>
2026-05-28 21:44:35 -0300

    Calculadora Básica

--- Program.cs
+++ Program.cs
@@ -1,38 +1,77 @@
-﻿int numero = 0, longitudNumero;
-string textoNumero;
-bool resultado;
+﻿int operacion, continuar = 0;
+string opcion;
+
+Console.WriteLine("***----- CALCULADORA BÁSICA -----***");
 
 do
 {
-    Console.WriteLine("Ingrese un número mayor a 0");
-    textoNumero = Console.ReadLine();
+    Console.WriteLine("\nSuma = 1; Resta = 2; Multiplicar = 3; Dividir = 4");
+    opcion = Console.ReadLine();
 
-    resultado = int.TryParse(textoNumero, out numero); //se guarda en valor numérico en la variable "numero"
-
-    if (resultado)
+    if (!int.TryParse(opcion, out operacion))
     {
-        if (numero <= 0 )
-        {
-            Console.WriteLine("\nEl numero ingresado es menor o igual a 0");
+        Console.WriteLine("\nValor ingresado no válido");
 
-        }else
-        {
-            //Obtengo la longitud del número ingresado
-            longitudNumero = textoNumero.Length;
-            string numeroInvertido = "";
-
-            //Invierto el número ingresado
-            for (int i = 0; i < longitudNumero; i++)
-            {
-                numeroInvertido = numeroInvertido + textoNumero[longitudNumero-1-i];
-            }
-
-            Console.WriteLine($"\nEl número invertido es: {numeroInvertido}");
-        }
-
+    }else if (operacion > 4 || operacion < 1)
+    {
+        Console.WriteLine("\nEl numero ingresado no se encuentra dentro de las opciones dadas");
+    
     }else
     {
-        Console.WriteLine("\nEl texto ingresado NO es un numero o es menor o igual 0\n");
+        double num1, num2, resultado;
+
+        Console.WriteLine("\nIngrese los numeros que desea calcular");
+        Console.WriteLine("\nNumero 1: ");
+
+        num1 = double.Parse(Console.ReadLine());
+       do
+       {
+            Console.WriteLine("\nNumero 2: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            switch (operacion)
+            {
+                case 1:
+                    resultado = num1 + num2;
+                    Console.WriteLine($"\n{num1} + {num2} = {resultado}");
+                    break;
+
+                case 2:
+                    resultado = num1 - num2;
+                    Console.WriteLine($"\n{num1} - {num2} = {resultado}");
+                    break;
+
+                case 3:
+                    resultado = num1 * num2;
+                    Console.WriteLine($"\n{num1} × {num2} = {resultado}");
+                    break;
+
+                case 4:
+                    
+                        if (num2 == 0)
+                        {
+                            Console.WriteLine("\nNo se puede dividir sobre 0. Ingrese un nuevo valor para el Numero2");
+                            
+                        }else
+                        {
+                            resultado = num1 / num2;
+                            Console.WriteLine($"\n{num1} ÷ {num2} = {resultado}");
+                        }
+                    
+                    break;
+            }
+       } while (operacion == 4 && num2 == 0);
+
+        Console.WriteLine("\n¿Desea realizar otra operación?");
+        
+        Console.WriteLine("\n SÍ = 0; NO = 1");
+        continuar = int.Parse(Console.ReadLine());
+
+        if (continuar == 1)
+        {
+            Console.WriteLine("\n¡Hasta Pronto!");
+        }
     }
 
-} while (!resultado || numero <= 0);
+} while (!int.TryParse(opcion, out operacion) || operacion > 4 || operacion < 1 || continuar == 0);
+
