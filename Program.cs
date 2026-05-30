de262ee65b19a39181caf887531ccb17508fad4c
Dana Ayelen Gauna <ayelengauna564@gmail.com>
2026-05-30 15:53:41 -0300

    Cacluladora mejorada. Se implementaron nuevas operaciones para realizar

--- Program.cs
+++ Program.cs
@@ -1,38 +1,136 @@
-﻿int numero = 0, longitudNumero;
-string textoNumero;
-bool resultado;
+﻿int operacion, continuar = 0;
+string opcion;
+double num1, resultado;
+
+Console.WriteLine("***----- CALCULADORA MEJORADA -----***");
 
 do
 {
-    Console.WriteLine("Ingrese un número mayor a 0");
-    textoNumero = Console.ReadLine();
+    Console.WriteLine("\n  OPERACIONES BÁSICAS:\nSuma = 1; Resta = 2; Multiplicar = 3; Dividir = 4");
+    Console.WriteLine("\n  OPERACIONES AVANZADAS:\nValor absoluto = 5; Cuadrado = 6; Raiz cuadrada = 7; Seno = 8; Coseno = 9; Parte entera de un tipo float = 0");
+    
+    Console.WriteLine("\nIngrese la opción que desea realizar:");
+    opcion = Console.ReadLine();
 
-    resultado = int.TryParse(textoNumero, out numero); //se guarda en valor numérico en la variable "numero"
-
-    if (resultado)
+    if (!int.TryParse(opcion, out operacion))
     {
-        if (numero <= 0 )
-        {
-            Console.WriteLine("\nEl numero ingresado es menor o igual a 0");
+        Console.WriteLine("\nValor ingresado no válido");
 
-        }else
-        {
-            //Obtengo la longitud del número ingresado
-            longitudNumero = textoNumero.Length;
-            string numeroInvertido = "";
+    }else if (operacion > 9 || operacion < 0)
+    {
+        Console.WriteLine("\nEl numero ingresado no se encuentra dentro de las opciones dadas");
+    
+    }else if (operacion >= 1 && operacion <= 4)
+    {
+        double num2;
 
-            //Invierto el número ingresado
-            for (int i = 0; i < longitudNumero; i++)
+        Console.WriteLine("\nIngrese los numeros que desea calcular");
+        Console.WriteLine("\nNumero 1: ");
+
+        num1 = double.Parse(Console.ReadLine());
+       do
+       {
+            Console.WriteLine("\nNumero 2: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            switch (operacion)
             {
-                numeroInvertido = numeroInvertido + textoNumero[longitudNumero-1-i];
-            }
+                case 1:
+                    resultado = num1 + num2;
+                    Console.WriteLine($"\n{num1} + {num2} = {resultado}");
+                    break;
 
-            Console.WriteLine($"\nEl número invertido es: {numeroInvertido}");
-        }
+                case 2:
+                    resultado = num1 - num2;
+                    Console.WriteLine($"\n{num1} - {num2} = {resultado}");
+                    break;
+
+                case 3:
+                    resultado = num1 * num2;
+                    Console.WriteLine($"\n{num1} × {num2} = {resultado}");
+                    break;
+
+                case 4:
+                    
+                        if (num2 == 0)
+                        {
+                            Console.WriteLine("\nNo se puede dividir sobre 0. Ingrese un nuevo valor para el Numero2");
+                            
+                        }else
+                        {
+                            resultado = num1 / num2;
+                            Console.WriteLine($"\n{num1} ÷ {num2} = {resultado}");
+                        }
+                    
+                    break;
+            }
+       } while (operacion == 4 && num2 == 0);
 
     }else
     {
-        Console.WriteLine("\nEl texto ingresado NO es un numero o es menor o igual 0\n");
+        Console.WriteLine("\nIngrese el numero que desea calcular");
+
+        do
+        {
+            Console.WriteLine("\nNumero 1: ");
+
+        num1 = double.Parse(Console.ReadLine());
+
+        switch (operacion)
+        {
+            case 0:
+                resultado = Math.Truncate(num1);
+                Console.WriteLine($"\n{num1} = {resultado}");
+                break;
+
+            case 5:
+                resultado = Math.Abs(num1); 
+                Console.WriteLine($"\n|{num1}| = {resultado}");
+                break;
+
+            case 6:
+                resultado = Math.Pow(num1, 2); 
+                Console.WriteLine($"\n{num1}² = {resultado}");
+                break;
+
+            case 7:
+                if (num1 < 0)
+                {
+                    Console.WriteLine("\nEl numero debe ser mayor o igual a 0. Ingrese un nuevo valor");
+                }
+                else
+                {
+                    resultado = Math.Sqrt(num1); 
+                    Console.WriteLine($"\n{num1}½ = {resultado}");
+                }
+                break;
+
+            case 8:
+                resultado = Math.Sin(num1); 
+                Console.WriteLine($"\nsen({num1}) = {resultado}");
+                break;
+
+            case 9:
+                resultado = Math.Cos(num1); 
+                Console.WriteLine($"\ncos({num1}) = {resultado}");
+                break;
+            
+        }
+        } while (operacion == 7 && num1 < 0);
     }
 
-} while (!resultado || numero <= 0);
+    if (!(operacion > 9 || operacion < 0))
+    {
+        Console.WriteLine("\n¿Desea realizar otra operación?");
+        
+        Console.WriteLine("\n SÍ = 0; NO = 1");
+        continuar = int.Parse(Console.ReadLine());
+
+        if (continuar == 1)
+        {
+            Console.WriteLine("\n¡Hasta Pronto!");
+        }
+    }
+
+
+} while ((!int.TryParse(opcion, out operacion) || operacion > 9 || operacion < 0) && continuar == 0);
