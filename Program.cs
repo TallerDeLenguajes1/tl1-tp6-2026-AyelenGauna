8064749e8b4baaa9af89d7caf86a0552f13dee16
Dana Ayelen Gauna <ayelengauna564@gmail.com>
2026-06-03 22:04:31 -0300

    El usuario ingresa una nueva cada distinta a la primera. Luego se la concatena con la esa misma

--- Program.cs
+++ Program.cs
@@ -43,7 +43,7 @@ do
 
 // CADENA DE TEXTO
 
-string cadena1;
+string cadena1, cadena2;
 int longCadena = 0, espacio = 0;
 
 Console.WriteLine("\n***----- CADENA DE TEXTO -----***");
@@ -56,6 +56,8 @@ do
     if (cadena1 != "")
     {
         longCadena = cadena1.Length;
+
+        espacio = 0;
 
         for (int i = 0; i < longCadena; i++)
         {
@@ -71,7 +73,7 @@ do
 
         }else
         {
-            Console.WriteLine($"\nLongitud de la cadena: {longCadena}");
+            Console.WriteLine($"\nLongitud de la primera cadena: {longCadena}");
             
         }
 
@@ -81,3 +83,50 @@ do
     }
 
 } while (cadena1 == "" || longCadena == espacio);
+
+
+
+do
+{
+    Console.WriteLine("\nIngrese otra cadena de texto:");
+    cadena2 = Console.ReadLine();
+    
+    if (cadena2 != "")
+    {
+        longCadena = cadena2.Length;
+
+        espacio = 0;
+
+        for (int i = 0; i < longCadena; i++)
+        {
+            if (cadena2[i] == ' ')
+            {
+                espacio ++;
+            }
+        }
+
+        if (espacio == longCadena)
+        {
+            Console.WriteLine("\nPor favor ingrese una cadena de texto nuevamente");
+
+        }else
+        {
+            if (string.Compare(cadena1, cadena2, true) != 0)
+            {
+                string cadenasConcat = string.Concat(cadena1, cadena2);
+
+                Console.WriteLine($"\nCadenas concatenadas: {cadenasConcat}");
+
+            }else
+            {
+                Console.WriteLine("\nLas cadenas son iguales. Por favor, ingrese una cadena de texto distinta a la primera cadena de texto");
+            }
+            
+        }
+
+    }else
+    {
+        Console.WriteLine("\nNo se ha ingresado ninguna cadena de texto. Por favor intentelo de nuevo");
+    }
+
+} while (cadena2 == "" || longCadena == espacio || string.Compare(cadena1, cadena2, true) == 0);
